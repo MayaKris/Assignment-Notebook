90fbc162b37410f4b7dbd45b1ffc45ad434c7c33
Maya Krishnan <mayark527@gmail.com>
2026-01-28 10:55:23 -0600

    Fixed list; created file

---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,13 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Maya Krishnan on 1/28/26.
+//
+
+import Foundation
+
+@Observable
+class AssignmentList {
+    
+}
--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -16,7 +16,14 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(assignmentItems) { item in
-                    Text(item.description)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course).font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
                 }
                 .onMove(perform: { indices, newOffset in
                     assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
@@ -24,10 +31,10 @@ struct ContentView: View {
                 .onDelete(perform: { indexSet in
                     assignmentItems.remove(atOffsets: indexSet)
                 })
-                .navigationBarItems(leading: EditButton())
             }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .navigationBarTitle("Assignment Notebook", displayMode: .inline)
     }
 }
 
